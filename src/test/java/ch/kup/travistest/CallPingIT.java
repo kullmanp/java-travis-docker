6c2fefe7e04baf7f7cfe666ad694e7921a64a950
Peter Kullmann <p.kullmann@arenae.ch>
2019-09-05 18:28:23 +0200

    Create failsafe integration test

---
+++ src/test/java/ch/kup/travistest/CallPingIT.java
@@ -0,0 +1,12 @@
+package ch.kup.travistest;
+
+import org.junit.jupiter.api.Test;
+
+public class CallPingIT {
+
+    @Test
+    public void callPing() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+
+}
