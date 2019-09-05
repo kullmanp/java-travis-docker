5fc9fa9762c2d241b7e1a131f5a38e7928b78c57
Peter Kullmann <p.kullmann@arenae.ch>
2019-09-05 19:30:18 +0200

    Integration test really connects to container

--- src/test/java/ch/kup/travistest/CallPingIT.java
+++ src/test/java/ch/kup/travistest/CallPingIT.java
@@ -2,11 +2,26 @@ package ch.kup.travistest;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CallPingIT {
 
     @Test
-    public void callPing() {
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    public void callPing() throws IOException {
+        String port = System.getProperty("PORT");
+        URL url = new URL("http://localhost:" + port + "/travistest/resources/ping");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        assertTrue(in.readLine().startsWith("Helloo"));
+        in.close();
     }
 
 }
